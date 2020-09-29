c241e710313a025dea0efce3755060e79cc6ee24
Roy Davis <>
2020-09-30 09:14:02 +1300

    Tidied up code, added env file, added git ignore

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express = require('express');
 
+// Get variables config from env file
+require('dotenv').config()
+const SERVER_PORT = process.env.SERVER_PORT;
+
 // Create a new express app instance
 const app: express.Application = express();
-    app.get('/', function (req, res) {
-    res.send('Working!');
+    app.get('/', (_req, res) => {
+    res.send({ status: 200, message: 'HTTP OK', });
 });
-    app.listen(3000, function () {
-    console.log('App is listening on port 3000!');
+
+// Configure ports
+app.listen(SERVER_PORT, () => {
+    console.log(`App is listening on port ${SERVER_PORT}!`);
 });
